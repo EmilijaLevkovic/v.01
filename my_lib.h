2233f465705127db2080168647d8c1d2daafca3a
EmilijaLevkovic <145105325+EmilijaLevkovic@users.noreply.github.com>
2023-09-25 22:59:12 +0300

    Update my_lib.h

--- my_lib.h
+++ my_lib.h
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <iomanip>
 using std::cout;
 using std::string;
 using std:: cin;
 using std::endl;
 using std::vector;
+using std::setprecision;
+using std::fixed;
 
 struct studentas {
   string Vardas;
