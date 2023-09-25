a0964a8c51b8e340763152ad4b75967e1e49c3da
EmilijaLevkovic <145105325+EmilijaLevkovic@users.noreply.github.com>
2023-09-25 23:14:07 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,25 +4,36 @@ int main() {
   studentas Laikinas;
   vector <studentas> grupe;
   for(int j=0;j+2;j++){
-    cout<<"Iveskite varda:";
+    cout<<"Įveskite vardą: ";
     cin>>Laikinas.Vardas;
-    cout<<"Iveskite pavarde:";
+    cout<<"Įveskite pavardę: ";
     cin>>Laikinas.Pavarde;
-    cout<<"Kiek ND pazymiu turi studentas?";
+    cout<<"Kiek ND pažymių turi studentas? ";
     int n;
     cin>>n;
     for (int i=0; i<n;i++) 
     {
       int k;
-      cout<<"Iveskite" <<i+1<<"ND Pazymi:";
+      cout<<"Įveskite" <<i+1<<" ND pažymį: ";
       cin>>k;
       Laikinas.ND_Pazymis.push_back(k);
     }
-    cout<<"Iveskite egzamino rezultata:";
+    cout<<"Įveskite egzamino rezultatą: ";
     cin>>Laikinas.Egzaminas;
+    double suma= 0;
+  for (int i=0; i<Laikinas.ND_Pazymis.size(); i++){
+    suma += Laikinas.ND_Pazymis[i];
+  }
+  double vidurkis= suma/ Laikinas.ND_Pazymis.size();
+  Laikinas.Galutinis= 0.4* vidurkis+ 0.6* Laikinas.Egzaminas;
     grupe.push_back(Laikinas);
     Laikinas.ND_Pazymis.clear();
+  cout<< "Galutinis balas: "<<fixed<<setprecision(2)<<Laikinas.Galutinis<<endl;
   }
-  for (auto&a: grupe)
-    cout<<a.Vardas<<" "<<a.Pavarde<<" "<<a.Egzaminas<<endl;;
+  for (auto&a: grupe){
+    cout<<a.Vardas<<" "<<a.Pavarde<<" Semestras:";
+    for (auto&z:a.ND_Pazymis)
+      cout<<z<<" ";
+    cout<<" Egzaminas:"a.Egzaminas<<endl;
+  }
 }
