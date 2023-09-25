c3f420b45c3d94a875b954bcb9bdbce6c701f290
EmilijaLevkovic <145105325+EmilijaLevkovic@users.noreply.github.com>
2023-09-25 17:18:56 +0300

    Biblioteka 1

---
+++ my_lib.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using std::cout;
+using std::string;
+using std:: cin;
+using std::endl;
+using std::vector;
+
+struct studentas {
+  string vard,pav;
+  vector <int> pazymis;
+  int egzaminas;
+  float rezultatas;
+};
