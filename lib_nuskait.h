57117a98ce1eb45e571499ffbeb2f3258cd12a3e
EmilijaLevkovic <145105325+EmilijaLevkovic@users.noreply.github.com>
2023-09-28 19:39:34 +0300

    Create lib_nuskait.h

---
+++ lib_nuskait.h
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iomanip>
+#include <algorithm>
+#include <limits>
+#include <random>
+#include <cstdlib>
+#include <fstream>
+
+using namespace std;
+
+struct studentas {
+  string Vardas,Pavarde;
+  vector <int> ND_Pazymis;
+  int Egzaminas;
+  double GalutinisVid;
+  double GalutinisMed;
+};
