0596192c8f6d447f5c08bdd02bf92190b379848a
EmilijaLevkovic <145105325+EmilijaLevkovic@users.noreply.github.com>
2023-09-30 15:18:17 +0300

    Update main_nuskait.cpp

--- main_nuskait.cpp
+++ main_nuskait.cpp
@@ -1,51 +1,64 @@
 #include "my_lib.h"
 
-double vidurkis(vector<int> v) {
-  double suma = 0;
-  for (int x : v) {
-    suma += x;
-  }
-  return suma / v.size();
+vector<studentas> nuskaityti_faila(const string& failo_vardas) {
+    ifstream fin(failo_vardas); 
+    if (!fin) { 
+        cout << "Nepavyko atidaryti failo " <<failo_vardas << endl;
+        return {}; 
+    }
+    string eilute;
+    getline(fin, eilute); 
+    stringstream ss(eilute);
+    string zodis;
+    int nd_kiekis = -2; 
+    while (ss >> zodis) { 
+        nd_kiekis++; 
+    }
+    vector<studentas> grupe; 
+    while (getline(fin, eilute)) {
+        stringstream ss(eilute); 
+        studentas Laikinas; 
+        ss >> Laikinas.Vardas >> Laikinas.Pavarde;
+        for (int i = 0; i < nd_kiekis; i++) { 
+            int k;
+            ss >> k;
+            Laikinas.ND_Pazymis.push_back(k);
+        }
+        ss >> Laikinas.Egzaminas; 
+        grupe.push_back(Laikinas); 
+    }
+    fin.close();
+    return grupe; 
 }
 
-double mediana(vector<int> v) {
-  sort(v.begin(), v.end());
-  int n = v.size();
-  if (n % 2 == 0) { 
-    return (v[n/2 - 1] + v[n/2]) / 2.0; 
-  } else { 
-    return v[n/2];
-  }
-}
+void isvesti_rezultatus(const vector<studentas>& grupe) {
+    cout << setw(15) << left << "Vardas" << setw(15) << left << "Pavarde" << setw(20) << right << "Galutinis (Vid.)" << setw(20) << right << "Galutinis (Med.)" << endl; 
+    cout << setfill('-') << setw(70) << "-" << endl; 
+    cout << setfill(' '); 
+    for (auto& Laikinas : grupe) { 
+    double suma = 0; 
+    for (int i = 0; i < Laikinas.ND_Pazymis.size(); i++) {
+        suma += Laikinas.ND_Pazymis[i]; 
+    }
+    double vidurkis = suma / Laikinas.ND_Pazymis.size();
+    Laikinas.GalutinisVid = 0.4 * vidurkis + 0.6 * Laikinas.Egzaminas; 
 
-studentas nuskaitytiStudenta(fstream& fin) {
-  studentas s; 
-  fin >> s.Vardas >> s.Pavarde; 
-  int x; 
-  while (fin >> x && x != -1) {
-    s.ND_Pazymis.push_back(x); 
-  }
-  s.Egzaminas = x;
-  return s;
-}
+        vector<int> pazymiai = Laikinas.ND_Pazymis;
+        sort(pazymiai.begin(), pazymiai.end());
+        double mediana;
+        if (pazymiai.size() % 2 == 0) {
+            mediana = (pazymiai[pazymiai.size() / 2] + pazymiai[pazymiai.size()/ 2 - 1]) / 2.0;
+        } else {
+            mediana = pazymiai[pazymiai.size() / 2];
+        }
+        Laikinas.GalutinisMed= 0.4 * mediana + 0.6 * Laikinas.Egzaminas;
 
-void spausdintiStudentus(vector<studentas> studentai) {
-  cout << "Vardas\t\tPavarde\t\tGalutinis (Vid.)\tGalutinis (Med.)\n"; 
-  cout << "-------------------------------------------\n";
-  for (studentas s : studentai) { 
-    s.GalutinisVid = 0.4 * vidurkis(s.ND_Pazymis) + 0.6 * s.Egzaminas;
-    s.GalutinisMed = 0.4 * mediana(s.ND_Pazymis) + 0.6 * s.Egzaminas;
-    cout << s.Vardas << "\t\t" << s.Pavarde << "\t\t" << fixed << setprecision(2) << s.GalutinisVid << "\t\t" << s.GalutinisMed << "\n";
-  }
+        cout << setw(15) << left << Laikinas.Vardas << setw(15) << left << Laikinas.Pavarde << setw(20) << right << fixed << setprecision(2) << Laikinas.GalutinisVid << setw(20) << right << fixed << setprecision(2) << Laikinas.GalutinisMed << endl; 
+    }
 }
 
 int main() {
-  fstream fin("kursiokai.txt");
-  vector<studentas> studentai;
-  while (!fin.eof()) { 
-    studentai.push_back(nuskaitytiStudenta(fin)); 
-  }
-  fin.close(); 
-spausdintiStudentus(studentai); 
+  vector<studentas> grupe= nuskaityti_faila("kursiokai.txt");
+  isvesti_rezultatus(grupe); 
   return 0;
 }
